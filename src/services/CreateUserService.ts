a2bc97c78f0da4784a8bc1425bde4858ef8f5125
Matheus Filipe <mfcr031@gmail.com>
2020-05-25 00:49:45 -0300

    add user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    // Não precisa criar outro repositório se os métodos não serão sobrescritos
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) {
+      throw new Error('Esse email já está em uso.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
