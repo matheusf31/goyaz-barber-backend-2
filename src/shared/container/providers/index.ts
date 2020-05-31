28c9b5308b0533312959a18b02e3d3756b1e030a
Matheus Filipe <mfcr031@gmail.com>
2020-05-31 16:12:59 -0300

    add comment

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStoragePrivider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider';
+
 container.registerSingleton<IStoragePrivider>(
   'StorageProvider',
   DiskStorageProvider,
