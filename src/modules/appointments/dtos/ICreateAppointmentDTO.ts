19e65784f80091caf21c82312cff88521bc5eb4c
Matheus Filipe <mfcr031@gmail.com>
2020-06-03 18:06:42 -0300

    add user_id, sevice, price

--- src/modules/appointments/dtos/ICreateAppointmentDTO.ts
+++ src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -1,4 +1,12 @@
 export default interface ICreateAppointmentDTO {
   provider_id: string;
+  user_id: string;
+  service:
+    | 'corte'
+    | 'corte e barba'
+    | 'barba'
+    | 'hot towel'
+    | 'corte e hot towel';
+  price: number;
   date: Date;
 }
