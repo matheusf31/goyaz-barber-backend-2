06ccae14d4eaccb9cc29bfdf0f1b8ab56cbbf216
Matheus Filipe <mfcr031@gmail.com>
2020-06-06 16:42:07 -0300

    add list users

---
+++ src/modules/users/services/ListUsersService.spec.ts
@@ -0,0 +1,48 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import ListUsersService from './ListUsersService';
+import CreateUserService from './CreateUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listUsers: ListUsersService;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
+
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+    listUsers = new ListUsersService(fakeUsersRepository);
+  });
+
+  it('should be able to list all users where provider false', async () => {
+    await createUser.execute({
+      name: 'John',
+      email: 'john@gmail.com',
+      password: '123123',
+      provider: true,
+    });
+
+    const user2 = await createUser.execute({
+      name: 'John',
+      email: 'johndoe@gmail.com',
+      password: '123123',
+      provider: false,
+    });
+
+    const user3 = await createUser.execute({
+      name: 'John',
+      email: 'johnthree@gmail.com',
+      password: '123123',
+      provider: false,
+    });
+
+    const users = await listUsers.execute();
+
+    expect(users).toEqual([user2, user3]);
+  });
+});
---
+++ src/modules/users/services/ListUsersService.ts
@@ -0,0 +1,21 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+@injectable()
+class ListUsersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute(): Promise<User[]> {
+    const users = await this.usersRepository.findAllUsers();
+
+    return users;
+  }
+}
+
+export default ListUsersService;
