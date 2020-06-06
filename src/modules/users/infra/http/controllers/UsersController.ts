a7cfe0a8c5951da1034fd82554a28976ee938a39
Matheus Filipe <mfcr031@gmail.com>
2020-06-06 16:43:39 -0300

    add list users

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -2,8 +2,17 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
+import ListUsersService from '@modules/users/services/ListUsersService';
 
 export default class UsersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listUsers = container.resolve(ListUsersService);
+
+    const users = await listUsers.execute();
+
+    return response.json(users);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email, password, provider } = request.body;
 
