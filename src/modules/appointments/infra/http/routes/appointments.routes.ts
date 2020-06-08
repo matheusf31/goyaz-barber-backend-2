6275c9dd361269598a7760f3683e372ea9db5d08
Matheus Filipe <mfcr031@gmail.com>
2020-06-08 15:55:53 -0300

    add new routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,18 +2,35 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentController from '../controllers/ProviderAppointmentController';
+import CancelAppointmentController from '../controllers/CancelAppointmentController';
+import ConcludedAppointmentController from '../controllers/ConcludedAppointmentController';
+import UserAppointmentsController from '../controllers/UserAppointmentsController';
 
 const appointmentsRouter = Router();
+
 const appointmentsController = new AppointmentsController();
+const providerAppointmentController = new ProviderAppointmentController();
+const cancelAppointmentController = new CancelAppointmentController();
+const concludedAppointmentController = new ConcludedAppointmentController();
+const userAppointmentsController = new UserAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
+appointmentsRouter.get('/', userAppointmentsController.index);
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.patch(
+  '/cancel/:appointment_id',
+  cancelAppointmentController.update,
+);
+
+appointmentsRouter.patch(
+  '/conclude/:appointment_id',
+  concludedAppointmentController.update,
+);
+
+appointmentsRouter.post('/provider', providerAppointmentController.create);
 
 export default appointmentsRouter;
