d904008421aa7a22a78d05c00575f39292bd0b99
Matheus Filipe <mfcr031@gmail.com>
2020-06-08 15:54:02 -0300

    add conclude appointment service

---
+++ src/modules/appointments/services/ConcludeAppointmentService.spec.ts
@@ -0,0 +1,59 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+import ConcludeAppointmentService from './ConcludeAppointmentService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointment: CreateAppointmentService;
+let concludeAppointment: ConcludeAppointmentService;
+
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    concludeAppointment = new ConcludeAppointmentService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to conclude a appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 4, 9, 10).getTime();
+    });
+
+    const appointment = await createAppointment.execute({
+      date: new Date(2020, 4, 13, 15),
+      provider_id: '123123',
+      user_id: 'logged-user',
+      service: 'corte',
+    });
+
+    await concludeAppointment.execute({
+      appointment_id: appointment.id,
+      concluded: true,
+    });
+
+    expect(appointment.concluded).toBe(true);
+
+    await concludeAppointment.execute({
+      appointment_id: appointment.id,
+      concluded: false,
+    });
+
+    expect(appointment.concluded).toBe(false);
+  });
+
+  it('should not be able to conclude a non existing appointment', async () => {
+    expect(
+      concludeAppointment.execute({
+        appointment_id: 'non-existing-appointment-id',
+        concluded: true,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/appointments/services/ConcludeAppointmentService.ts
@@ -0,0 +1,41 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  appointment_id: string;
+  concluded: boolean;
+}
+
+@injectable()
+class ConcludeAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    appointment_id,
+    concluded,
+  }: IRequest): Promise<Appointment> {
+    const appointment = await this.appointmentsRepository.findById(
+      appointment_id,
+    );
+
+    if (!appointment) {
+      throw new AppError('O agendamento não foi encontrado.');
+    }
+
+    appointment.concluded = concluded;
+
+    await this.appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default ConcludeAppointmentService;
