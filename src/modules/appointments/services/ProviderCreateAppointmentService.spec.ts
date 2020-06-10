b27ac70817ab5712ced8af9f47883abd9da396fc
Matheus Filipe <mfcr031@gmail.com>
2020-06-10 15:34:17 -0300

    provider create a appointment

---
+++ src/modules/appointments/services/ProviderCreateAppointmentService.spec.ts
@@ -0,0 +1,116 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ProviderCreateAppointmentService from './ProviderCreateAppointmentService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let providerCreateAppointment: ProviderCreateAppointmentService;
+
+describe('CreateAppointment', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    providerCreateAppointment = new ProviderCreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to create a new appointment by provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 4, 9, 8).getTime();
+    });
+
+    const appointment = await providerCreateAppointment.execute({
+      date: new Date(2020, 4, 9, 15),
+      provider_id: '123123',
+      user_id: 'logged-user',
+      service: 'corte',
+    });
+
+    const appointment2 = await providerCreateAppointment.execute({
+      date: new Date(2020, 4, 9, 16),
+      provider_id: 'provider2-id',
+      service: 'corte e barba',
+      foreign_client_name: 'Jose Da Silva',
+    });
+
+    await providerCreateAppointment.execute({
+      date: new Date(2020, 4, 9, 9),
+      provider_id: 'provider3-id',
+      user_id: 'user3-id',
+      service: 'barba',
+    });
+
+    await providerCreateAppointment.execute({
+      date: new Date(2020, 4, 9, 10, 30),
+      provider_id: 'provider4-id',
+      user_id: 'user4-id',
+      service: 'corte e hot towel',
+    });
+
+    await providerCreateAppointment.execute({
+      date: new Date(2020, 4, 9, 11, 30),
+      provider_id: 'provider5-id',
+      user_id: 'user5-id',
+      service: 'hot towel',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment).toHaveProperty('additionals');
+    expect(appointment.provider_id).toBe('123123');
+    expect(appointment.user_id).toBe('logged-user');
+    expect(appointment.service).toBe('corte');
+    expect(appointment.price).toBe(25);
+
+    expect(appointment2.foreign_client_name).toBe('Jose Da Silva');
+    expect(appointment2).toHaveProperty('additionals');
+
+    await expect(
+      providerCreateAppointment.execute({
+        date: new Date(2020, 4, 9, 14, 30),
+        provider_id: 'provider5-id',
+        user_id: 'user5-id',
+        service: 'any for test',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a new appointment at the same time as another', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date(2020, 4, 11, 14).getTime();
+    });
+
+    const appointmentDate = new Date(2020, 4, 11, 16);
+
+    await providerCreateAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+      user_id: 'logged-user',
+      service: 'corte e barba',
+    });
+
+    await expect(
+      providerCreateAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+        user_id: 'logged-user',
+        service: 'barba',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create a appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 12, 13).getTime();
+    });
+
+    await expect(
+      providerCreateAppointment.execute({
+        date: new Date(2020, 4, 12, 14),
+        provider_id: 'same-id',
+        user_id: 'same-id',
+        service: 'corte e hot towel',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ src/modules/appointments/services/ProviderCreateAppointmentService.ts
@@ -0,0 +1,90 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  user_id?: string;
+  service:
+    | 'corte'
+    | 'corte e barba'
+    | 'barba'
+    | 'hot towel'
+    | 'corte e hot towel'
+    | 'any for test';
+  date: Date;
+  foreign_client_name?: string;
+}
+
+@injectable()
+class ProviderCreateAppointmentService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    user_id,
+    service,
+    date,
+    foreign_client_name,
+  }: IRequest): Promise<Appointment> {
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
+      date,
+      provider_id,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('Esse horário já está ocupado.');
+    }
+
+    if (user_id === provider_id) {
+      throw new AppError('Você não pode marcar um agendamento consigo mesmo.');
+    }
+
+    let price: number;
+
+    switch (service) {
+      case 'corte':
+        price = 25;
+        break;
+
+      case 'barba':
+        price = 18;
+        break;
+
+      case 'corte e barba':
+        price = 35;
+        break;
+
+      case 'hot towel':
+        price = 25;
+        break;
+
+      case 'corte e hot towel':
+        price = 45;
+        break;
+
+      default:
+        throw new AppError('Serviço inválido.');
+    }
+
+    const appointment = await this.appointmentsRepository.create({
+      provider_id,
+      user_id,
+      service,
+      price,
+      date,
+      foreign_client_name,
+    });
+
+    return appointment;
+  }
+}
+
+export default ProviderCreateAppointmentService;
