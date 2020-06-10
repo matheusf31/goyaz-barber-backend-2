f89e294603832134b63eb5130146651880ec54af
Matheus Filipe <mfcr031@gmail.com>
2020-06-10 09:34:15 -0300

    add new routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,27 +3,29 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentController from '../controllers/ProviderAppointmentController';
-import CancelAppointmentController from '../controllers/CancelAppointmentController';
+import UserCancelAppointmentController from '../controllers/UserCancelAppointmentController';
 import ConcludedAppointmentController from '../controllers/ConcludedAppointmentController';
 import UserAppointmentsController from '../controllers/UserAppointmentsController';
+import AppointmentsInfoController from '../controllers/AppointmentsInfoController';
 
 const appointmentsRouter = Router();
 
 const appointmentsController = new AppointmentsController();
 const providerAppointmentController = new ProviderAppointmentController();
-const cancelAppointmentController = new CancelAppointmentController();
+const userCancelAppointmentController = new UserCancelAppointmentController();
 const concludedAppointmentController = new ConcludedAppointmentController();
 const userAppointmentsController = new UserAppointmentsController();
+const appointmentsInfoController = new AppointmentsInfoController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', userAppointmentsController.index);
-
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.delete('/:appointment_id', appointmentsController.delete);
 
 appointmentsRouter.patch(
   '/cancel/:appointment_id',
-  cancelAppointmentController.update,
+  userCancelAppointmentController.update,
 );
 
 appointmentsRouter.patch(
@@ -32,5 +34,7 @@ appointmentsRouter.patch(
 );
 
 appointmentsRouter.post('/provider', providerAppointmentController.create);
+
+appointmentsRouter.get('/info', appointmentsInfoController.index);
 
 export default appointmentsRouter;
