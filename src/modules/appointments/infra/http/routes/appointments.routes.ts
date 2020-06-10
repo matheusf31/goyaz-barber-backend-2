ba52d710479178a3aae80a51d3b49d14ff78a1d0
Matheus Filipe <mfcr031@gmail.com>
2020-06-10 15:43:37 -0300

    add new routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,31 +1,27 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import AppointmentsController from '../controllers/AppointmentsController';
 import ProviderAppointmentController from '../controllers/ProviderAppointmentController';
-import UserCancelAppointmentController from '../controllers/UserCancelAppointmentController';
 import ConcludedAppointmentController from '../controllers/ConcludedAppointmentController';
 import UserAppointmentsController from '../controllers/UserAppointmentsController';
 import AppointmentsInfoController from '../controllers/AppointmentsInfoController';
+import AdditionalController from '../controllers/AdditionalController';
 
 const appointmentsRouter = Router();
 
-const appointmentsController = new AppointmentsController();
 const providerAppointmentController = new ProviderAppointmentController();
-const userCancelAppointmentController = new UserCancelAppointmentController();
 const concludedAppointmentController = new ConcludedAppointmentController();
 const userAppointmentsController = new UserAppointmentsController();
 const appointmentsInfoController = new AppointmentsInfoController();
+const additionalController = new AdditionalController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', userAppointmentsController.index);
-appointmentsRouter.post('/', appointmentsController.create);
-appointmentsRouter.delete('/:appointment_id', appointmentsController.delete);
-
+appointmentsRouter.post('/', userAppointmentsController.create);
 appointmentsRouter.patch(
   '/cancel/:appointment_id',
-  userCancelAppointmentController.update,
+  userAppointmentsController.update,
 );
 
 appointmentsRouter.patch(
@@ -34,7 +30,13 @@ appointmentsRouter.patch(
 );
 
 appointmentsRouter.post('/provider', providerAppointmentController.create);
+appointmentsRouter.delete(
+  '/:appointment_id',
+  providerAppointmentController.delete,
+);
 
 appointmentsRouter.get('/info', appointmentsInfoController.index);
+
+appointmentsRouter.put('/additional', additionalController.update);
 
 export default appointmentsRouter;
