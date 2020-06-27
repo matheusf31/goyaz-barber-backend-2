1f194f9af5c21537ee2833012484e98a076f1903
Matheus Filipe <mfcr031@gmail.com>
2020-06-27 17:16:46 -0300

    🔧 coverage 100%

--- src/modules/appointments/services/UpdateAppointmentAdditionalsService.spec.ts
+++ src/modules/appointments/services/UpdateAppointmentAdditionalsService.spec.ts
@@ -31,6 +31,17 @@ describe('UpdateAppointmentAdditionals', () => {
       additional: {
         description: 'cerveja',
         value: 7.5,
+        quantity: 1,
+      },
+    });
+
+    await updateAppointmentAdditionals.execute({
+      appointment_id: appointment.id,
+      provider_id: 'provider-id',
+      additional: {
+        description: 'cerveja',
+        value: 7.5,
+        quantity: 1,
       },
     });
 
@@ -42,7 +53,7 @@ describe('UpdateAppointmentAdditionals', () => {
 
     expect(additionalService).toHaveProperty('description');
     expect(additionalService).toHaveProperty('value');
-    expect(appointmentAdditionals.additionals.total_income).toBe(7.5);
+    expect(appointmentAdditionals.additionals.total_income).toBe(15);
   });
 
   it('should not be able to update additionals from a non existing appointment', async () => {
@@ -53,6 +64,29 @@ describe('UpdateAppointmentAdditionals', () => {
         additional: {
           description: 'cerveja',
           value: 7.5,
+          quantity: 1,
+        },
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update additionals from a invalid quantity', async () => {
+    const appointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      date: new Date(2020, 4, 5, 14, 0, 0),
+      user_id: 'any',
+      service: 'corte',
+      price: 25,
+    });
+
+    await expect(
+      updateAppointmentAdditionals.execute({
+        appointment_id: appointment.id,
+        provider_id: 'provider-id',
+        additional: {
+          description: 'cerveja',
+          value: 7.5,
+          quantity: 0,
         },
       }),
     ).rejects.toBeInstanceOf(AppError);
@@ -74,6 +108,39 @@ describe('UpdateAppointmentAdditionals', () => {
         additional: {
           description: 'cerveja',
           value: 7.5,
+          quantity: 1,
+        },
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to update appointment additionals with same description but with different values', async () => {
+    const appointment = await fakeAppointmentsRepository.create({
+      provider_id: 'provider-id',
+      date: new Date(2020, 4, 5, 14, 0, 0),
+      user_id: 'any',
+      service: 'corte',
+      price: 25,
+    });
+
+    updateAppointmentAdditionals.execute({
+      appointment_id: appointment.id,
+      provider_id: 'provider-id',
+      additional: {
+        description: 'cerveja',
+        value: 7.5,
+        quantity: 1,
+      },
+    });
+
+    await expect(
+      updateAppointmentAdditionals.execute({
+        appointment_id: appointment.id,
+        provider_id: 'provider-id',
+        additional: {
+          description: 'cerveja',
+          value: 8,
+          quantity: 1,
         },
       }),
     ).rejects.toBeInstanceOf(AppError);
