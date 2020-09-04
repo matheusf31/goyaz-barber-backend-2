f3b9586b77fdc3690574d504449a4b5fd85b5ad9
Matheus Filipe <mfcr031@gmail.com>
2020-09-03 22:11:13 -0300

    update ddd rule

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -27,7 +27,7 @@ usersRouter.post(
       email: Joi.string().email().required(),
       phone: Joi.string()
         .required()
-        .pattern(/^(62|062)(\d{4,5}-?\d{4})$/),
+        .pattern(/^(\d\d)(\d{4,5}-?\d{4})$/),
       password: Joi.string().required(),
       provider: Joi.boolean(),
       admin: Joi.boolean(),
